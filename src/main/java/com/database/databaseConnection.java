97ce7d97b6d2644e7579d3a0d2279076cb78e227
SERGIO MASSO GIRALDO <sergio.masso@uao.edu.co>
2025-04-06 13:18:06 -0500

    Agregando lógica al database.java

--- src/main/java/com/database/databaseConnection.java
+++ src/main/java/com/database/databaseConnection.java
@@ -1,4 +1,20 @@
 package com.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class databaseConnection {
+    private  static  final  String URL = "jdbc:mysql://localhost:3306/base_proyecto";
+    private  static  final  String USER = "root";
+    private  static  final  String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Carga del Drive
+            return  DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (ClassNotFoundException e){
+            throw new SQLException("Error:No se encontro el drive de Mysql", e);
+        }
+    }
 }
